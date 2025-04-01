35f97a4380d9a9ca7a822a9998df8d6fee4060b7
3liezerDev <3dgar3liezer@gmail.com>
2025-04-02 01:51:38 +0200

    refactor: simplify PhoneCard rendering and conditional styles for Xiaomi models

--- src/features/phones/components/phoneCard/PhoneCard.jsx
+++ src/features/phones/components/phoneCard/PhoneCard.jsx
@@ -2,6 +2,11 @@ import "./PhoneCard.scss";
 import { PHONE_FIELDS } from "../../constants/apiFields";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import {
+  overlayVariants,
+  fadeVariant,
+  instantFade,
+} from "../phoneCard/phoneCard.animations";
 
 export const PhoneCard = ({ phone }) => {
   const id = phone[PHONE_FIELDS.ID];
@@ -16,10 +21,7 @@ export const PhoneCard = ({ phone }) => {
       >
         <motion.div
           className="phone-card__overlay"
-          variants={{
-            rest: { height: "0%" },
-            hover: { height: "100%" },
-          }}
+          variants={overlayVariants}
           transition={{ duration: 0.4, ease: [0.15, 1, 0.5, 1] }}
         />
 
@@ -27,70 +29,26 @@ export const PhoneCard = ({ phone }) => {
           <img
             src={phone[PHONE_FIELDS.IMAGE_URL]}
             alt={phone[PHONE_FIELDS.NAME]}
-            className="phone-card__image"
+            className={`${
+              phone[PHONE_FIELDS.BRAND].toUpperCase() === "XIAOMI"
+                ? "xiaomi-image"
+                : "phone-card__image"
+            }`}
           />
         </header>
 
         <main className="phone-card__main">
           <div className="phone-card__info">
-            <motion.span
-              className="phone-card__brand"
-              variants={{
-                rest: {
-                  color: "#000",
-                  y: 0,
-                  opacity: 1,
-                  transition: { duration: 0.1 }, // ← texto a negro
-                },
-                hover: {
-                  color: "#fff",
-                  y: 0,
-                  opacity: 1,
-                  transition: { duration: 0.05 }, // ← texto a blanco (más rápido)
-                },
-              }}
-            >
+            <motion.span className="phone-card__brand" variants={fadeVariant}>
               {phone[PHONE_FIELDS.BRAND].toUpperCase()}
             </motion.span>
 
-            <motion.span
-              className="phone-card__name"
-              variants={{
-                rest: {
-                  color: "#000",
-                  y: 0,
-                  opacity: 1,
-                  transition: { duration: 0.001 },
-                },
-                hover: {
-                  color: "#fff",
-                  y: 0,
-                  opacity: 1,
-                  transition: { duration: 0.001  },
-                },
-              }}
-            >
+            <motion.span className="phone-card__name" variants={instantFade}>
               {phone[PHONE_FIELDS.NAME].toUpperCase()}
             </motion.span>
           </div>
 
-          <motion.span
-            className="phone-card__price"
-            variants={{
-              rest: {
-                color: "#000",
-                y: 0,
-                opacity: 1,
-                transition: { duration: 0.001  },
-              },
-              hover: {
-                color: "#fff",
-                y: 0,
-                opacity: 1,
-                transition: { duration: 0.001  },
-              },
-            }}
-          >
+          <motion.span className="phone-card__price" variants={instantFade}>
             {phone[PHONE_FIELDS.BASE_PRICE]} EUR
           </motion.span>
         </main>
