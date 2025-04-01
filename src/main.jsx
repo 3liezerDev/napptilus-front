ddbbc1e87f7ea0dca57ff6c8ddd3490e8f5ee378
3liezerDev <3dgar3liezer@gmail.com>
2025-04-02 01:51:00 +0200

    chore: install React Query and configure QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.scss'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>
   </BrowserRouter>,
 )
