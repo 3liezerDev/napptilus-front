54e81d22c762b46bf3a0a89c15e6454eed90c6c2
3liezerDev <3dgar3liezer@gmail.com>
2025-04-02 13:08:27 +0200

    feat(context): implement CartContext using Context API and localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import AppRouter from "./router/AppRouter";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CartProvider } from "@/contexts/CartContext";
+
 import "./styles/global.scss";
 
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <AppRouter />;
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <CartProvider>
+          <AppRouter />
+        </CartProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './styles/global.scss'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-  <QueryClientProvider client={queryClient}>
+  <>
+ 
     <App />
-  </QueryClientProvider>
-  </BrowserRouter>,
+  
+  </>,
 )
